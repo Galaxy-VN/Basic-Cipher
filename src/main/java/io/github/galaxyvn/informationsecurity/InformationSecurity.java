c7454956103f72d74aec7fefc792bb6860e2db97
Phạm Gia Huy <52485153+Galaxy-VN@users.noreply.github.com>
2025-11-26 10:17:02 +0700

    Add files via upload

---
+++ src/main/java/io/github/galaxyvn/informationsecurity/InformationSecurity.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.galaxyvn.informationsecurity;
+
+import io.github.galaxyvn.informationsecurity.week1.CaeserFrame;
+
+/**
+ *
+ * @author Administrator
+ */
+public class InformationSecurity {
+
+    public static void main(String[] args) {
+        CaeserFrame frame = new CaeserFrame();
+        frame.setVisible(true);
+    }
+}
